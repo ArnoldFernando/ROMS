2b0c405ace3ae886d2b66e46c55644968f022917
Arnold Fernando <junfernando727@gmail.com>
2025-06-04 21:05:32 +0800

    feat: Implement authentication views and styles
    - Added login, registration, password reset, and email confirmation views.
    - Created a layout for the application with Bootstrap integration.
    - Introduced SCSS variables for styling and imported Bootstrap styles.
    - Updated routes to include authentication routes and home redirection.
    - Configured Vite to compile SCSS files.

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- Scripts -->
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+
+    <div id="app">
+
+        @extends('adminlte::page')
+
+        @section('content')
+            <main class="py-4">
+                {{ $slot }}
+            </main>
+        @stop
+    </div>
+
+
+
+    @yield('js')
+
+
+    @stack('scripts')
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.min.js"></script>
+
+</body>
+
+</html>
