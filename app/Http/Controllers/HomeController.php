230173e32caa844b06537adc16322fae23a9f75b
Arnold Fernando <junfernando727@gmail.com>
2025-06-05 13:26:39 +0800

    feat: Enhance search functionality in HomeController with additional query parameters

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ArchivedFile;
+use App\Models\File;
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -21,8 +24,67 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $query = $request->input('query');
+
+        $allfiles = File::count();
+        $archivedfiles = ArchivedFile::count();
+        $folders = Folder::count();
+
+        $searchResults = null;
+
+        if ($query) {
+            $files = File::with('folder')->where(function ($q) use ($query) {
+                $q->where('document_code', 'like', "%$query%")
+                    ->orWhere('subject', 'like', "%$query%")
+                    ->orWhere('originating_office', 'like', "%$query%")
+                    ->orWhere('remarks', 'like', "%$query%")
+                    ->orWhere('file', 'like', "%$query%")
+                    ->orWhere('date', 'like', "%$query%");
+            })->get();
+
+            $matchedFolders = Folder::where(function ($q) use ($query) {
+                $q->where('name', 'like', "%$query%")
+                    ->orWhere('description', 'like', "%$query%");
+            })->get();
+
+            $searchResults = compact('files', 'matchedFolders', 'query');
+        }
+
+        return view('admin.dashboard', compact('allfiles', 'archivedfiles', 'folders', 'searchResults'));
+    }
+
+
+    public function liveSearch(Request $request)
+    {
+        $query = $request->get('query');
+
+        // Get matching files
+        $files = File::with('folder')->where(function ($q) use ($query) {
+            $q->where('document_code', 'like', "%$query%")
+                ->orWhere('subject', 'like', "%$query%")
+                ->orWhere('originating_office', 'like', "%$query%")
+                ->orWhere('remarks', 'like', "%$query%")
+                ->orWhere('file', 'like', "%$query%")
+                ->orWhere('date', 'like', "%$query%");
+        })->get();
+
+        // Add file_url to each file
+        foreach ($files as $file) {
+            $file->file_url = asset('storage/' . $file->file);
+            // assuming 'file' is the actual filename
+        }
+
+        // Get matching folders
+        $matchedFolders = Folder::where(function ($q) use ($query) {
+            $q->where('name', 'like', "%$query%")
+                ->orWhere('description', 'like', "%$query%");
+        })->get();
+
+        return response()->json([
+            'files' => $files,
+            'folders' => $matchedFolders
+        ]);
     }
 }
